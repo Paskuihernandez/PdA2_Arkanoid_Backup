ee47a83d12e2ff2a1a0788ed2f2ea72a68ad7e45
Paskuihernandez <paskuihernandez@gmail.com>
2018-09-25 17:18:35 +0200

    Martes_25/09

---
+++ PdA2_Android/Assets/Scripts/Ball.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    int direccionX;
+    int direccionY;
+
+    float speed;
+
+
+	// Use this for initialization
+	void Start () {
+
+        speed = Random.Range(10, 15);
+
+        direccionX = Random.Range(0, 2);
+        if (direccionX == 0)
+        {
+            direccionX = 1;
+        }
+        else
+        {
+            direccionX = -1;
+        }
+
+        direccionY = Random.Range(0, 2);
+        if (direccionY == 0)
+        {
+            direccionY = 1;
+        }
+        else
+        {
+            direccionY = -1;
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed * direccionX, speed * direccionY);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ PdA2_Android/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    public GameObject Player1;
+    public GameObject Player2;
+
+    public float movSpeed = 300;
+
+    private Rigidbody2D playerBody;
+    private float ScreenWidth;
+    private float ScreenHeight;
+
+    // Use this for initialization
+    void Start () {
+        ScreenWidth = Screen.width;
+        ScreenHeight = Screen.height;
+        playerBody = Player1.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        int i = 0;
+
+        while(i < Input.touchCount)
+        {
+            if(Input.GetTouch (i).position.y > ScreenHeight / 2)
+            {
+                RunCharacter(1.0f);
+            }
+            if(Input.GetTouch(i).position.y < ScreenHeight / 2)
+            {
+                RunCharacter(-1.0f);
+            }
+            i++;
+        }
+
+    }
+
+    private void RunCharacter(float verticalInput)
+    {
+        playerBody.AddForce(new Vector2(verticalInput * movSpeed * Time.deltaTime, 0));
+    }
+
+}
